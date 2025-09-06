ae6eda749fa5172a258f1e9792ff4b0cb00848d0
Filip Hric <filip@filiphric.sk>
2025-09-06 22:49:10 +0200

    remove retries

--- playground/playwright.config.ts
+++ playground/playwright.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
   timeout: 5000,
   fullyParallel: false,
   forbidOnly: !!process.env.CI,
-  retries: process.env.CI ? 2 : 0,
+  retries: 0,
   workers: process.env.CI ? 1 : undefined,
   reporter: 'html',
   use: {
